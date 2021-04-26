858708c33fea06edfbf689126de9eed707a79b96
DMyrzich <myrzich@bk.ru>
2021-04-26 19:38:32 +0300

    edit my content

--- src/components/Heder/Heder.js
+++ src/components/Heder/Heder.js
@@ -5,7 +5,7 @@ const Heder = () => {
 
     return (<nav className="blue darken-1">
         <div className="nav-wrapper">
-            <Link to="/shop/" className="brand-logo">Киномания</Link>
+            <Link to="/shop/" className="brand-logo">Магазин</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to="/shop/">Товары</Link></li>
                 <li><Link to="/shop/">Акции</Link></li>
