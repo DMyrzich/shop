447b7183f4e12ed1972c2daace11cff812905569
DMyrzich <myrzich@bk.ru>
2021-04-26 20:12:42 +0300

    l

--- src/components/Heder/Heder.js
+++ src/components/Heder/Heder.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Heder = () => {
 
     return (<nav className="blue darken-1">
         <div className="nav-wrapper">
-            <Link to="/shop/" className="brand-logo">Магазин</Link>
+            <Link to="/shop/" className="brand-logo">Мой Магазин</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to="/shop/">Товары</Link></li>
                 <li><Link to="/shop/">Акции</Link></li>
